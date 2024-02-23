654731dbe16a329984b9959d8d7fd9e1f98c3a86
rakshitha-h-s <rakshithahs040@gmail.com>
2024-02-23 17:19:59 +0530

    testng added

---
+++ src/test/java/tests/TestExample.java
@@ -0,0 +1,50 @@
+package tests;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestExample {
+	@Test
+	public void testExample() {
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getHeader("content-type"));
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+
+	@Test
+	void test_2() {
+		baseURI = "https://reqres.in/api";
+		given().get("users?page=2").then().statusCode(200).body("data[1].id", equalTo(8));
+
+	}
+
+	@Test
+	void test_3() {
+		baseURI = "https://reqres.in/api";
+		Map<String, Object> map1 = new HashMap();
+		map1.put("\"name\"", "Rakshitha");
+		map1.put("\"address\"", "Hassan");
+		//System.out.println(map1);
+		JSONObject request = new JSONObject();
+		request.put("name", "Rakshitha");
+		request.put("address", "Hassan");
+		//System.out.println(request.toJSONString());
+		given().body(request.toJSONString()).when().post("/users").then().statusCode(201);
+
+	}
+}
